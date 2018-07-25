ab54acd7b19a92a6e397e561282dbedb624a39fd
Fabian Luna <flb031@gmail.com>
2018-07-25 00:50:40 -0500

    [TEST-004] Add accordion component

---
+++ app/src/components/App/Accordion/Accordion.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { string, bool } from 'prop-types';
+import classNames from 'classnames';
+import styles from './Accordion.scss';
+
+class Accordion extends Component {
+
+    state = {
+        show: false
+    }
+
+    toogle = () => {
+        this.setState({
+            show: !this.state.show
+        });
+    }
+
+    componentWillMount() {
+        this.setState({
+            show: this.props.startOpen
+        })
+    }
+
+    render() {
+
+        const classToogle = classNames({[styles.container__open]: this.state.show});
+
+        return (
+            <div className={`${styles.container} ${this.props.className} ${classToogle}`}>
+                <div
+                    className={styles.container__header}
+                    onClick={this.toogle}>
+                    <img
+                        className={styles.icon}
+                        src={this.props.icon}
+                        alt={this.props.title}/>
+    
+                    <span className={styles.title}>{this.props.title}</span>
+                </div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+Accordion.defaultProps = {
+    className: '',
+    startOpen: true
+};
+
+Accordion.propTypes = {
+    icon: string.isRequired,
+    title: string.isRequired,
+    className: string,
+    startOpen: bool
+};
+
+export default Accordion;
--- app/src/components/App/App.jsx
+++ app/src/components/App/App.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import './App.scss';
+import Accordion from './Accordion';
+
+import styles from './App.scss';
+import sharedStyles from 'Styles/index.scss';
+
+import searchIcon from 'Assets/icons/filters/search.svg';
+import starIcon from 'Assets/icons/filters/star.svg';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+
+                <div className={`${sharedStyles.grid} ${sharedStyles.p}`}>
+                    <div className={`${sharedStyles.grid__item} ${sharedStyles['one-quarter']}`}>
+                        <div className={styles.title_filter}>
+                            <strong>Filtros</strong>
+                        </div>
+
+                        <Accordion
+                            icon={searchIcon}
+                            title="Nombre de hotel"
+                            className={sharedStyles['m-quarter-t']}>
+                            input here
+                        </Accordion>
+
+                        <Accordion
+                            icon={starIcon}
+                            title="Estrellas"
+                            className={sharedStyles['m-quarter-t']}>
+                            stars here
+                        </Accordion>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
