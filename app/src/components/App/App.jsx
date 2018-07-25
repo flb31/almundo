5c3304b7a83dc15d7be37e35fbd0e532c059c11a
Fabian Luna <flb031@gmail.com>
2018-07-25 14:29:57 -0500

    [TEST-005] Add responsive for hotelcard component

--- app/src/components/App/App.jsx
+++ app/src/components/App/App.jsx
@@ -13,8 +13,8 @@ class App extends Component {
             <div>
                 <Header />
 
-                <div className={`${sharedStyles.grid} ${sharedStyles['mid--p']} ${sharedStyles['grid-negative']}`}>
-                    <aside className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']} ${sharedStyles['mid--one-quarter']}`}>
+                <div className={`${sharedStyles.grid} ${sharedStyles['mid--p']}`}>
+                    <aside className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']} ${sharedStyles['mid--one-quarter']} ${sharedStyles['p-l-none']}`}>
                         <Box className={`${sharedStyles['phone--hidden']} ${sharedStyles['mid--show-block']}`}>
                             <strong>Filtros</strong>
                         </Box>
@@ -22,10 +22,10 @@ class App extends Component {
                         <FilterContainer sharedStyles={sharedStyles} />
                     </aside>
 
-                    <section className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']}  ${sharedStyles['mid--three-quarters']} ${sharedStyles['p']} ${sharedStyles['p-double-l']} ${sharedStyles['mid--p-v-none']}  ${sharedStyles['mid--p-r-none']} ${sharedStyles['mid--p-l']}`}>
-                        <HotelCard className={sharedStyles['m-half-b']} />
-                        <HotelCard className={sharedStyles['m-half-b']} />
-                        <HotelCard className={sharedStyles['m-half-b']} />
+                    <section className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']}  ${sharedStyles['mid--three-quarters']} ${sharedStyles['p-v']} ${sharedStyles['p-half-h']} ${sharedStyles['mid--p-v-none']}  ${sharedStyles['mid--p-r-none']} ${sharedStyles['mid--p-l']}`}>
+                        <HotelCard className={sharedStyles['m-half-b']} sharedStyles={sharedStyles} />
+                        <HotelCard className={sharedStyles['m-half-b']} sharedStyles={sharedStyles} />
+                        <HotelCard className={sharedStyles['m-half-b']} sharedStyles={sharedStyles} />
                     </section>
                 </div>
 
--- app/src/components/App/Hotel/HotelCard/HotelCard.jsx
+++ app/src/components/App/Hotel/HotelCard/HotelCard.jsx
@@ -4,23 +4,24 @@ import Button from 'Components/App/Button';
 
 import stylesButton from 'Components/App/Button/Button.scss';
 import styles from './HotelCard.scss';
-import sharedStyles from 'Styles/index.scss';
 
 import starIcon from 'Assets/icons/filters/star.svg';
 import amenityIcon from 'Assets/icons/amenities/bathtub.svg';
 
 const HotelCard = (props) => {
+    const {sharedStyles} = props;
+
     return (
         <article className={`${styles.hotel} ${props.className}`}>
-            <Box className={`${sharedStyles.grid} ${sharedStyles['p-half']}`}>
-                <figure className={`${sharedStyles.grid__item} ${sharedStyles['one-third']} ${sharedStyles['p-l-none']} `}>
+            <Box className={`${sharedStyles.grid} ${sharedStyles['tablet--p-half']}`}>
+                <figure className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']} ${sharedStyles['tablet--one-third']} ${sharedStyles['p-l-none']} `}>
                     <img
                         className={styles.image}
                         src="http://via.placeholder.com/300x200"
                         alt="Main Hotel" />
                 </figure>
 
-                <summary className={`${sharedStyles.grid__item} ${sharedStyles['one-third']} ${sharedStyles['p-half-l']} ${styles.summary}`}>
+                <summary className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']} ${sharedStyles['tablet--one-third']} ${sharedStyles['p-half-l']} ${styles.summary}`}>
                     <h1>Hotel Stefanos</h1>
                     <div className={styles.stars}>
                         <img src={starIcon} alt="Star" />
@@ -35,9 +36,9 @@ const HotelCard = (props) => {
                     </div>
                 </summary>
 
-                <div className={`${sharedStyles.grid__item} ${sharedStyles['one-third']} ${sharedStyles['p-double-l']} ${styles.summary} ${styles.price}`} >
+                <div className={`${sharedStyles.grid__item} ${sharedStyles['one-whole']} ${sharedStyles['tablet--one-third']} ${sharedStyles['p-half-h']} ${sharedStyles['tablet--p-double-l']} ${styles.price}`} >
                     <div className={styles.container}>
-                        <p>Precio por noche por <br /> habitación</p>
+                        <p>Precio por noche por habitación</p>
                         <h1>ARS <b>863</b></h1>
                         <Button className={stylesButton.big}>Ver hotel</Button>
                     </div>
